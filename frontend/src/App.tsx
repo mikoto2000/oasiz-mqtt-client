3707c59cc81601b23565199b0fd4896de383c819
mikoto2000 <mikoto2000@gmail.com>
2023-03-20 00:12:35 +0900

    状態を流し込めるように修正。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, MouseEvent } from 'react'
 import { MqttClient, QoS, MqttConnection, PublishMessage, SubscribeTopic, ReceivedMessage, MqttClientProps } from './MqttClient'
 import './App.css'
 
@@ -27,8 +28,7 @@ function App() {
 
   const DEFAULT_RECEIVED_MESSAGES : Array<ReceivedMessage> = [];
 
-
-  const mqttClientProps: MqttClientProps = {
+  const DEFAULT_MQTT_CONTEXT: MqttClientProps = {
     INITIAL_CONNECTION: DEFAULT_MQTT_CONNECTION,
     INITIAL_PUBLISH_MESSAGE: DEFAULT_PUBLISH_MESSAGE,
     INITIAL_ADD_SUBSCRIBE_TOPIC: DEFAULT_ADD_SUBSCRIBE_TOPIC,
@@ -36,9 +36,50 @@ function App() {
     INITIAL_RECEIVED_MESSAGES: DEFAULT_RECEIVED_MESSAGES,
   }
 
+  const FIRST_MQTT_CONTEXT: MqttClientProps = {
+    ...DEFAULT_MQTT_CONTEXT,
+    INITIAL_CONNECTION: {...DEFAULT_MQTT_CONNECTION,
+      username: 'pubuser',
+      password: 'password',
+    }
+  }
+
+  const SECOND_MQTT_CONTEXT: MqttClientProps = {
+    ...DEFAULT_MQTT_CONTEXT,
+    INITIAL_CONNECTION: {...DEFAULT_MQTT_CONNECTION,
+      username: 'subuser',
+      password: 'password',
+    },
+    INITIAL_SUBSCRIBING_TOPICS: new Map<string, SubscribeTopic>(
+      [
+        ["testtopic1/#", { name: "testtopic1/#", qos: 0 }],
+        ["testtopic2/#", { name: "testtopic2/#", qos: 2 }],
+      ]),
+    INITIAL_RECEIVED_MESSAGES: [
+      { topic: "testtopic1/1", payload: "test" },
+      { topic: "testtopic1/2", payload: "tetest" },
+      { topic: "testtopic2/1", payload: "tetest" },
+    ],
+  }
+
+  const [currentMqttContext, setCurrentMqttContext] = useState<MqttClientProps>(DEFAULT_MQTT_CONTEXT);
+  const [mqttContexts, setMqttContexts] = useState<Map<string, MqttClientProps>>(
+    new Map<string, MqttClientProps>([['pubuser', FIRST_MQTT_CONTEXT], ['subuser', SECOND_MQTT_CONTEXT]]));
+
+  const handleContextClick = (event: MouseEvent<HTMLAnchorElement>, contextName: string) => {
+    event.preventDefault();
+    console.debug(`handleContextClick: ${contextName}`);
+    let newContext = mqttContexts.get(contextName) || DEFAULT_MQTT_CONTEXT;
+    setCurrentMqttContext((prevContext) => newContext);
+  }
+
   return (
     <div className="App">
-      <MqttClient {...mqttClientProps}></MqttClient>
+      <h1>contexts:</h1>
+      <ol>
+        {[...mqttContexts.keys()].map((key) => <li key={key}><a href="#" onClick={(event) => handleContextClick(event, key)}>{key}</a></li>)}
+      </ol>
+      <MqttClient key={JSON.stringify(currentMqttContext)} {...currentMqttContext}></MqttClient>
     </div>
   )
 }
